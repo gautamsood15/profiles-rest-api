c82f177608d0792af40259253c80a4511a435b7e
gautamsood15 <gauty22@gmail.com>
2021-04-10 18:01:51 +0530

    creating a simple viewset 3

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,4 +1,4 @@
-f rom rest_framework.views import APIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
