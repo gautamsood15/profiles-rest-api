23d587ea82df7cb82b7c63ec490f6fafeefae96d
gautamsood15 <gauty22@gmail.com>
2021-04-30 18:44:44 +0530

    DUMCOMM 2

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -10,6 +10,7 @@ router.register('profile', views.UserProfileViewSet)
 router.register('feed', views.UserProfileFeedViewSet)
 
 
+
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
     path('login/', views.UserLoginApiView.as_view()),
