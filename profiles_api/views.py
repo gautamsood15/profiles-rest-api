4434f253d76b9bbb6842aa1d70b1309b45a34d67
gautamsood15 <gauty22@gmail.com>
2021-05-05 18:49:42 +0530

    Add authentication and permissions to viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,9 +2,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+
 
 from profiles_api import serializers
 from profiles_api import models
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -103,3 +106,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
