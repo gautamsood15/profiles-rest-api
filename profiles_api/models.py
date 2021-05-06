022551c4f14b4aef022b842335139a022eb2fac1
gautamsood15 <gauty22@gmail.com>
2021-05-06 19:26:12 +0530

    Add new model item

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -62,4 +62,12 @@ class ProfileFeedItem(models.Model):
     """Profile Status Update"""
     user_profile = models.ForeignKey(
         settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
     )
+
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as a string"""
+        return self.status_text
