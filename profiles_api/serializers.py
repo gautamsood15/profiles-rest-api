8230fd6218bde54a36f05041e68afbe315e1eb22
gautamsood15 <gauty22@gmail.com>
2021-05-07 19:47:50 +0530

    Create profile feed item serializer

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -38,4 +38,6 @@ class ProfileFeedITemSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {'user_profile': {'read_only': True}}
         
