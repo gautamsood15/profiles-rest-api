79ffaa84d3a312d0680134de5dce1bcffcb709f2
gautamsood15 <gauty22@gmail.com>
2021-05-08 18:37:16 +0530

    Restrict viewing status updates to logged in users only

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,7 +6,8 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
+
 
 from profiles_api import serializers
 from profiles_api import models
@@ -125,10 +126,7 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     serializer_class = serializers.ProfileFeedITemSerializer
     queryset = models.ProfileFeedItem.objects.all()
-    permission_classes = (
-        permissions.UpdateOwnStatus,
-        IsAuthenticatedOrReadOnly
-    )
+    permission_classes = (permissions.UpdateOwnStatus, IsAuthenticated)
 
 
     def perform_create(self, serializer):
